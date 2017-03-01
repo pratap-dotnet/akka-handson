bf6e0e244dc8532be30813c703be1186da4d410f
Pratap Bhaskar <pratap.aditi@outlook.com>
2017-03-01 23:20:05 +0530

    lesson 6 - actor cycle

--- file-actors/TailActor.cs
+++ file-actors/TailActor.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Akka.Actor;
 
 namespace file_actors
@@ -12,16 +8,19 @@ namespace file_actors
     {
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
+        }
 
-            _observer = new FileObserver(Self, filePath);
+        protected override void PreStart()
+        {
+            _observer = new FileObserver(Self, _filePath);
             _observer.Start();
 
             _fileStream = new FileStream(Path.GetFullPath(_filePath),
@@ -30,6 +29,14 @@ namespace file_actors
 
             var text = _fileStreamReader.ReadToEnd();
             Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
         }
 
         protected override void OnReceive(object message)
